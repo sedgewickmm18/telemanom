84de9b2568d83d0ff4a7f2cd52587af44431cd57
Markus Müller <markus_mueller@de.ibm.com>
2020-01-28 19:49:47 +0100

    update

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name='mmfunctions',
+    version='0.0.2',
+    packages=find_packages(),
+    install_requires=requirements
+)
